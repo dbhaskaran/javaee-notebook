9c7c9157066e96fec5eeb015dec069f9cfa15be9
Deepak Bhaskaran <dbhaskaran@opentable.com>
2019-04-04 14:24:46 -0700

    Added lombok

--- jersey-rest-basics/src/main/java/org/dbhaskaran/jersey_rest_basics/Contact.java
+++ jersey-rest-basics/src/main/java/org/dbhaskaran/jersey_rest_basics/Contact.java
@@ -2,7 +2,14 @@ package org.dbhaskaran.jersey_rest_basics;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @XmlRootElement
+@Getter
+@Setter
+@NoArgsConstructor
 public class Contact {
 
 	private int person_id;
@@ -10,46 +17,10 @@ public class Contact {
 	private String last_name;
 	private String country;
 
-	public Contact() {
-
-	}
-
 	public Contact(int person_id, String first_name, String last_name, String country) {
 		this.person_id = person_id;
 		this.first_name = first_name;
 		this.last_name = last_name;
-		this.country = country;
-	}
-
-	public int getPerson_id() {
-		return person_id;
-	}
-
-	public void setPerson_id(int person_id) {
-		this.person_id = person_id;
-	}
-
-	public String getFirst_name() {
-		return first_name;
-	}
-
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
-	}
-
-	public String getLast_name() {
-		return last_name;
-	}
-
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
 		this.country = country;
 	}
 
