d6b27a9750ea916c77995dedf9d29225d0ce8f96
Deepak Bhaskaran <dbhaskaran@opentable.com>
2019-04-08 16:37:28 -0700

    Updated Deprecated method call

--- hibernate-basics/src/main/java/org/dbhaskaran/hibernate_basics/App.java
+++ hibernate-basics/src/main/java/org/dbhaskaran/hibernate_basics/App.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
  * Hello world!
@@ -13,12 +15,13 @@ public class App {
 	public static void main(String[] args) {
 		Person p1 = new Person();
 		p1.setId(45);
-		p1.setFirstName("Emily");
-		p1.setLastName("McDowell");
-		p1.setCountry("Spain");
+		p1.setFirstName("Sam");
+		p1.setLastName("Tolety");
+		p1.setCountry("Italy");
 
 		Configuration con = new Configuration().configure().addAnnotatedClass(Person.class);
-		SessionFactory sf = con.buildSessionFactory();
+		ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+		SessionFactory sf = con.buildSessionFactory(reg);
 		Session session = sf.openSession();
 		Transaction txn = session.beginTransaction();
 		session.save(p1);
